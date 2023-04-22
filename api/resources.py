d8c5492d4c4c66731456444ddfc5e67484dfbebe
moradi.saeed <moradi.saeed@hasti.co>
2023-04-22 17:49:35 +0330

    Add basic authentication to resources

--- api/resources.py
+++ api/resources.py
@@ -1,8 +1,22 @@
 from flask_restful import Resource
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+import config
+
+auth = HTTPBasicAuth()
+users = config.users
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
 
 
 # This is example API Resource
+
 class Weather(Resource):
+    @auth.login_required
     def get(self):
         """
         Route get method
--- config.py
+++ config.py
@@ -1,7 +1,16 @@
+from werkzeug.security import generate_password_hash
+
+users = {
+        "admin": generate_password_hash("password"),
+        "user": generate_password_hash("pass")
+    }
+
 class Config(object):
     DEBUG = False
     TESTING = False
 
 
+
 class ProductionConfig(Config):
     DEBUG = True
+
